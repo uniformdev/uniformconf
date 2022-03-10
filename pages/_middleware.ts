020fddd01424b15152c414b60b2f39602e0cb9dc
Alex Shyba <57730703+uniform-demo@users.noreply.github.com>
2022-03-10 13:30:08 -0800

    fix: disable browser caching for pages and ignore static paths from middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,10 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { createEdgeContext } from "@uniformdev/context-edge";
 import { createUniformEdgeMiddleware } from "@uniformdev/context-edge-vercel";
 import manifest from "../lib/context/manifest.json";
+import { ManifestV2 } from "@uniformdev/context";
+
+const IGNORED_PATHS = /^\/.*\.(ico|png|jpg|jpeg|svg)$/g;
 
 export default async function middleware(request: NextRequest) {
   if (process.env.NODE_ENV === "development") {
@@ -16,18 +19,32 @@ export default async function middleware(request: NextRequest) {
     });
   }
 
-  console.log("url: " + process.env.VERCEL_URL);
+  if (
+    request.method.toUpperCase() !== "GET" ||
+    request.nextUrl.pathname.match(IGNORED_PATHS)
+  ) {
+    return NextResponse.next();
+  }
 
-  // @ts-ignore
-  const context = createEdgeContext({ manifest, request });
+  const context = createEdgeContext({
+    manifest: manifest as ManifestV2,
+    request,
+  });
 
   const middleware = createUniformEdgeMiddleware();
 
-  const response = middleware({
+  const response = await middleware({
     context,
     origin: new URL(`https://${process.env.VERCEL_URL}`),
     request,
   });
 
-  return response;
+  return new Response(response.body, {
+    ...response,
+    headers: {
+      ...response.headers,
+      "Cache-Control": "no-store, must-revalidate",
+      Expires: "0",
+    },
+  });
 }
