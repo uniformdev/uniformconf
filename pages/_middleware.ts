eabad7e740108c5e7f28baef238267dde07b7c36
Kam Figy <kam.figy@uniform.dev>
2022-03-14 11:47:24 -0700

    feat(stackblitz): enable repo to come online with stackblitz with no configuration
    Also disabled edge rendering by default, but left it commented to show how to enable.

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,50 +1,56 @@
-import { NextRequest, NextResponse } from "next/server";
-import { createEdgeContext } from "@uniformdev/context-edge";
-import { createUniformEdgeMiddleware } from "@uniformdev/context-edge-vercel";
-import manifest from "../lib/context/manifest.json";
-import { ManifestV2 } from "@uniformdev/context";
+// import { NextRequest, NextResponse } from "next/server";
+// import { createEdgeContext } from "@uniformdev/context-edge";
+// import { createUniformEdgeMiddleware } from "@uniformdev/context-edge-vercel";
+// import manifest from "../lib/context/manifest.json";
+// import { ManifestV2 } from "@uniformdev/context";
 
-const IGNORED_PATHS = /^\/.*\.(ico|png|jpg|jpeg|svg)$/g;
+// const IGNORED_PATHS = /^\/.*\.(ico|png|jpg|jpeg|svg)$/g;
 
-export default async function middleware(request: NextRequest) {
-  if (process.env.NODE_ENV === "development") {
-    console.log("Middleware disabled in development");
-    return;
-  }
+// export default async function middleware(request: NextRequest) {
+//   if (process.env.NODE_ENV === "development") {
+//     console.log("Middleware disabled in development");
+//     return;
+//   }
 
-  if (!process.env.VERCEL_URL) {
-    console.error("VERCEL_URL environment is not defined");
-    return new Response("Configuration Error", {
-      status: 500,
-    });
-  }
+//   if (!process.env.VERCEL_URL) {
+//     console.error("VERCEL_URL environment is not defined");
+//     return new Response("Configuration Error", {
+//       status: 500,
+//     });
+//   }
 
-  if (
-    request.method.toUpperCase() !== "GET" ||
-    request.nextUrl.pathname.match(IGNORED_PATHS)
-  ) {
-    return NextResponse.next();
-  }
+//   if (
+//     request.method.toUpperCase() !== "GET" ||
+//     request.nextUrl.pathname.match(IGNORED_PATHS)
+//   ) {
+//     return NextResponse.next();
+//   }
 
-  const context = createEdgeContext({
-    manifest: manifest as ManifestV2,
-    request,
-  });
+//   const context = createEdgeContext({
+//     manifest: manifest as ManifestV2,
+//     request,
+//   });
 
-  const middleware = createUniformEdgeMiddleware();
+//   const middleware = createUniformEdgeMiddleware();
 
-  const response = await middleware({
-    context,
-    origin: new URL(`https://${process.env.VERCEL_URL}`),
-    request,
-  });
+//   const response = await middleware({
+//     context,
+//     origin: new URL(`https://${process.env.VERCEL_URL}`),
+//     request,
+//   });
 
-  return new Response(response.body, {
-    ...response,
-    headers: {
-      ...response.headers,
-      "Cache-Control": "no-store, must-revalidate",
-      Expires: "0",
-    },
-  });
+//   return new Response(response.body, {
+//     ...response,
+//     headers: {
+//       ...response.headers,
+//       "Cache-Control": "no-store, must-revalidate",
+//       Expires: "0",
+//     },
+//   });
+// }
+
+// faux middleware; remove this and uncomment the above code to enable
+// edge rendering (requires a paid Uniform account)
+export default async function middleware() {
+  return;
 }
