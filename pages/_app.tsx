4ae3791cd50582a9a10bfa6cfaa6c813219f9489
Uniform Demo <alex.shyba@uniform.dev>
2022-04-25 18:20:07 -0700

    feat: edge-side context execution for vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,10 @@ export default function UniformConfApp({
   serverUniformContext,
 }: UniformAppProps) {
   return (
-    <UniformContext context={serverUniformContext ?? clientContext}>
+    <UniformContext
+      context={serverUniformContext ?? clientContext}
+      outputType="edge"
+    >
       <Component {...pageProps} />
     </UniformContext>
   );
