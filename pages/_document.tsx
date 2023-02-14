41b67598401b308f20e98877a9a1a808e5dcc5e7
Alex Shyba <alexs@uniform.dev>
2023-02-14 15:25:16 -0800

    chore: removing ssr personalization function as we are running static mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,74 +1,45 @@
-import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from "next/document";
-import { enableNextSsr } from "@uniformdev/context-next";
-import { createUniformContext } from "../lib/context/uniformContext";
+import { Html, Head, Main, NextScript } from "next/document";
 import getConfig from "next/config";
 
-const {
-  publicRuntimeConfig: {
-    gaTrackingId,
-  },
-} = getConfig();
-
-class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const serverTracker = createUniformContext(ctx);
-    enableNextSsr(ctx, serverTracker);
-    return await Document.getInitialProps(ctx);
+export default function Document() {
+  const {
+    publicRuntimeConfig: { gaTrackingId },
+  } = getConfig();
+  let tagManagerSrc = "";
+  if (gaTrackingId) {
+    tagManagerSrc = `https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`;
   }
-
-  render(): React.ReactElement {
-    let tagManagerSrc= '';
-    if (gaTrackingId)
-    {
-      tagManagerSrc = `https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`
-    }
-
-    return (
-      <Html lang="en">
-        <Head>
-          <link href="/favicon/favicon.ico" rel="icon" />
-          <link
-            href="/favicon/apple-touch-icon.png"
-            rel="apple-touch-icon"
+  return (
+    <Html lang="en">
+      <Head>
+        <link href="/favicon/favicon.ico" rel="icon" />
+        <link href="/favicon/apple-touch-icon.png" rel="apple-touch-icon" />
+        <meta
+          name="description"
+          content="UniformConf, a Uniform content demo site"
+        />
+      </Head>
+      {tagManagerSrc && (
+        <>
+          <script async src={tagManagerSrc}></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaTrackingId}', {
+                page_path: window.location.pathname,
+              });
+              `,
+            }}
           />
-          <meta
-            name="description"
-            content="UniformConf, a Uniform content demo site"
-          />
-        </Head>
-        {tagManagerSrc && (
-          <>
-            <script async src={tagManagerSrc}></script>
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaTrackingId}', {
-                  page_path: window.location.pathname,
-                });
-                `
-              }}
-            />
-          </>
-        )}
-        <body className="leading-normal tracking-normal text-white gradient">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        </>
+      )}
+      <body className="leading-normal tracking-normal text-white gradient">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
