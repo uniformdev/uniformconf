2003d36c7828154744e0f5dd78cdf845a96aa977
Alex Shyba <alexs@uniform.dev>
2023-02-14 16:00:48 -0800

    feat: enhancer stubbs

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,8 +1,14 @@
 import getConfig from "next/config";
 import { createPreviewHandler } from "@uniformdev/canvas-next";
+import runEnhancers from "lib/enhancers/enhancers";
+
+const context = {
+  preview: true,
+};
 
 const handler = createPreviewHandler({
   secret: () => getConfig().serverRuntimeConfig.previewSecret,
+  enhance: (composition) => runEnhancers(composition, context),
 });
 
 export default handler;
