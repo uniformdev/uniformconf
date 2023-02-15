eb02baf8ed6e8605180d4536ed1036b5bae7824c
Artem Miroshnyk <artemm@uniform.dev>
2023-02-15 17:36:33 +0100

    Upgraded to 18.11.0 uniform and add some comments

--- pages/[[...id]].tsx
+++ pages/[[...id]].tsx
@@ -1,7 +1,64 @@
-import { withUniformGetStaticProps } from "@uniformdev/canvas-next/slug";
-export { getStaticPaths } from "@uniformdev/canvas-next/slug";
+import { withUniformGetStaticProps, withUniformGetStaticPaths } from "@uniformdev/canvas-next/slug";
 import MainContainer from "@/components/MainContainer";
+
+export default MainContainer;
 
 export const getStaticProps = withUniformGetStaticProps({ param: "id" });
 
-export default MainContainer;
+// const getStaticProps = async (context: GetStaticPropsContext) => {
+//   const canvasClient = new CanvasClient({
+//     apiKey: process.env.UNIFORM_API_KEY,
+//     apiHost: process.env.UNIFORM_CLI_BASE_URL,
+//     projectId: process.env.UNIFORM_PROJECT_ID,
+//   });
+//
+//   const slug = context?.params?.id;
+//   const { preview } = context;
+//   const slugString = Array.isArray(slug) ? slug.join("/") : slug;
+//   const slashedSlug = !slugString
+//     ? "/"
+//     : slugString.startsWith("/")
+//       ? slugString
+//       : `/${slugString}`;
+//
+//   const { composition } = await canvasClient.getCompositionBySlug({
+//     slug,
+//     state: process.env.NODE_ENV === "development"
+//       ? CANVAS_DRAFT_STATE
+//       : CANVAS_PUBLISHED_STATE,
+//   });
+//
+//   return {
+//     props: {
+//       composition,
+//       preview: Boolean(preview),
+//     },
+//   };
+// }
+
+
+export const getStaticPaths = withUniformGetStaticPaths();
+
+// const getStaticPaths = async () => {
+//   const canvasClient = new CanvasClient({
+//     apiKey: process.env.UNIFORM_API_KEY,
+//     apiHost: process.env.UNIFORM_CLI_BASE_URL,
+//     projectId: process.env.UNIFORM_PROJECT_ID,
+//   });
+//
+//   const pages = await canvasClient.getCompositionList({
+//     skipEnhance: true,
+//     state: process.env.NODE_ENV === "development"
+//       ? CANVAS_DRAFT_STATE
+//       : CANVAS_PUBLISHED_STATE,
+//   });
+//
+//   const paths = pages.compositions
+//     .filter((c) => c.composition._slug)
+//     .map((c) =>
+//       c.composition._slug?.startsWith("/")
+//         ? `${c.composition._slug}`
+//         : `/${c.composition._slug}`
+//     );
+//   return { paths, fallback: true };
+// }
