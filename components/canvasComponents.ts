07ee96b67c9d60d29fb26544908306b27efb90eb
Artem Miroshnyk <artemm@uniform.dev>
2023-02-16 11:57:05 +0100

    Fix canvas component reference

--- components/canvasComponents.ts
+++
@@ -1,8 +0,0 @@
-// IMPORTANT: all Canvas managed components have to be imported here in order to be discovered by the <UniformSlot /> and rendered:
-
-import "./Hero";
-import "./Footer";
-import "./Navbar";
-import "./RegisterForm";
-import "./TalkList";
-import "./WhyAttend";
